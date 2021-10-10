c8c16d278634cd3398ccecd432c398b9a2fc3545
wahyu alfarisi <wahyualfarisi@gmail.com>
2021-10-10 15:05:01 +0700

    add query params

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { useState, useEffect  } from 'react';
 import './App.css'
 import Paginations from './components/Paginations';
+import { useLocation, useHistory } from 'react-router-dom';
 
 function App() {
+
+  const history = useHistory();
+  const search  = useLocation().search;
+  const page = new URLSearchParams(search).get('page');
+
 
   const [isLoading, setIsLoading] = useState(false);
   const [ data, setData ] = useState([]);
   const [ meta, setMeta] = useState(null);
-  const [currPage, setCurrentPage] = useState(1);
-
+  const [currPage, setCurrentPage] = useState( page ? +page : 1 );
 
   useEffect( () => {
-
+    
     setIsLoading(true);
     fetch(`https://zlzew.mocklab.io/pagination?page=${currPage}`)
       .then(res => {
@@ -25,42 +30,44 @@ function App() {
       })
       .catch(err => {
           setIsLoading(false);
-          console.log(err)
       })
-
-
-
+      
   }, [currPage] )
 
+  const onChangePageHandler = ( num ) => {
+    setCurrentPage(num)
+    history.push(`?page=${num}`)
+  }
   
 
   return (
-    <div className="App">
-      <ul className="collection">
-        {data.length > 0 && data.map((item, i) => {
-            return (
-              <li className="collection-item" key={i}>
-                <img 
-                  src={item.img_url}
-                  alt=""
-                />
-            </li>
-            )
-        })}
-      </ul>
+    
+      <div className="App">
+        <ul className="collection">
+          {data.length > 0 && data.map((item, i) => {
+              return (
+                <li className="collection-item" key={i}>
+                  <img 
+                    src={item.img_url}
+                    className={isLoading ? `blur` : ``}
+                    alt=""
+                  />
+              </li>
+              )
+          })}
+        </ul>
 
-      {meta && (
-        <Paginations 
-            totalCount={meta.total}
-            pageSize={meta.limit}
-            onPageChange={(page) => setCurrentPage(page)}
-            siblingCount={1}
-            currentPage={currPage}
-        />
-      )}
-      
-
-    </div>
+        {meta && (
+          <Paginations 
+              totalCount={meta.total}
+              pageSize={meta.limit}
+              onPageChange={(page) => onChangePageHandler(page)}
+              siblingCount={1}
+              currentPage={currPage}
+          />
+        )}
+      </div>
+    
   );
 }
 
