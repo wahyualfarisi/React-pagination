8e5897378f304ca849edb12376d51260c565c517
wahyu alfarisi <wahyualfarisi@gmail.com>
2021-10-10 16:38:37 +0700

    add test pagination

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,48 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { pagination } from './hooks/usePagination';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('pagination', () => {
+    it('should return empty array', () => {
+      let results = pagination();
+      expect(results).toBeUndefined();
+    });
+
+    it('should return [1,2,3,4]', () => {
+      let results = pagination(7, 2, 1, 1);
+      expect(results).toEqual([1,2,3,4])
+    })
+    it('should return [1]', () => {
+      let results = pagination(2, 2, 1, 1);
+      expect(results).toEqual([1])
+    })
+
+    it('should return [1,2,3,4,5, right]', () => {
+      let results1 = pagination(14, 2, 1, 1);
+      expect(results1).toEqual([1,2,3,4,5,'right'])
+
+      let results2 = pagination(14, 2, 1, 2);
+      expect(results2).toEqual([1,2,3,4,5,'right'])
+
+      let results3 = pagination(14, 2, 1, 3);
+      expect(results3).toEqual([1,2,3,4,5,'right'])
+    })
+
+    it('should return [left,2,3,4,5,6,right] on current page 4', () => {
+      let results1 = pagination(14, 2, 1, 4);
+      expect(results1).toEqual(['left',2,3,4,5,6,'right'])
+    })
+
+    it('should return [left,3,4,5,6,7] on current page 5', () => {
+      let results1 = pagination(14, 2, 1, 5);
+      expect(results1).toEqual(['left',3,4,5,6,7])
+    })
+
+    it('should return [left,3,4,5,6,7] on current page 6', () => {
+      let results1 = pagination(14, 2, 1, 6);
+      expect(results1).toEqual(['left',3,4,5,6,7])
+    })
+
+    it('should return [left,3,4,5,6,7] on current page 7', () => {
+      let results1 = pagination(14, 2, 1, 7);
+      expect(results1).toEqual(['left',3,4,5,6,7])
+    })
 });
--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -8,6 +8,45 @@ const range = (start, end) => {
 export const NAV_LEFT = 'left';
 export const NAV_RIGHT = 'right';
 
+export const pagination = ( totalCount = null, pageSize = null, siblingCount = null, currentPage = null ) => {
+    
+    if( !totalCount && !pageSize && !siblingCount && !currentPage) return undefined;
+
+    const totalPageCount = Math.ceil( totalCount / pageSize );
+    const totalPageNumbers = siblingCount + 5;
+    if( totalPageNumbers >= totalPageCount){
+        return range(1, totalPageCount);
+    }
+
+    const leftSiblingIndex = Math.max( currentPage - siblingCount, 1 );
+    const rightSiblingIndex = Math.min( currentPage + siblingCount , totalPageCount);
+      
+
+    const shouldShowNavLeft = leftSiblingIndex > 2;
+    const shouldShowNavRight = rightSiblingIndex < (totalPageCount - 1);
+
+
+    if(!shouldShowNavLeft && shouldShowNavRight){
+        let leftItemCount = 5;
+        let leftRange = range(1, leftItemCount);
+        return [...leftRange, NAV_RIGHT]
+    }
+
+
+    if(shouldShowNavLeft && shouldShowNavRight){
+        let start = leftSiblingIndex - 1;
+        let middleRange = range(leftSiblingIndex, rightSiblingIndex);
+        let end = rightSiblingIndex + 1;
+        return [NAV_LEFT, start, ...middleRange, end, NAV_RIGHT];
+    }
+
+    if(shouldShowNavLeft && !shouldShowNavRight){
+        let rightItemCount = 5;
+        let rightRange = range( (totalPageCount - rightItemCount) + 1 , totalPageCount );
+        return [NAV_LEFT, ...rightRange];
+    }
+}
+
 export const usePagination = ({
     totalCount,
     pageSize,
@@ -16,41 +55,7 @@ export const usePagination = ({
  }) => {
 
     const paginationRange = useMemo( () => {
-
-        const totalPageCount = Math.ceil( totalCount / pageSize );
-
-        const totalPageNumbers = siblingCount + 5;
-
-        if( totalPageNumbers >= totalPageCount){
-            return range(1, totalPageCount);
-        }
-
-        const leftSiblingIndex = Math.max( currentPage - siblingCount, 1 );
-        const rightSiblingIndex = Math.min( currentPage + siblingCount , totalPageCount);
-
-        const shouldShowLeftDots = leftSiblingIndex > 2;
-        const shouldShowRightDots = rightSiblingIndex < totalPageCount - 1;
-
-        if(!shouldShowLeftDots && shouldShowRightDots){
-            let leftItemCount = 5 * siblingCount;
-            let leftRange = range(1, leftItemCount);
-            return  [...leftRange, NAV_RIGHT];
-        }
-
-        if(shouldShowLeftDots && !shouldShowRightDots){
-            
-            let rightItemCount = 5 * siblingCount;
-            let rightRange = range( totalPageCount - rightItemCount + 1, totalPageCount );
-
-            return [NAV_LEFT, ...rightRange];
-        }
-
-        if(shouldShowLeftDots && shouldShowRightDots){
-            
-            let middleRange = range(leftSiblingIndex, rightSiblingIndex);
-            return [NAV_LEFT, leftSiblingIndex - 1, ...middleRange, rightSiblingIndex + 1, NAV_RIGHT];
-        }
-
+        return pagination(totalCount, pageSize, siblingCount, currentPage);
     }, [totalCount, pageSize, siblingCount, currentPage])
 
     return paginationRange;
